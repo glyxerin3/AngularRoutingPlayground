6f37501ec79a5f7422c9060e5321b8d045765bc1
glyxerin3 <glyxkind@gmx.de>
2020-11-04 10:54:34 +0100

    Angular Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OverviewComponent} from "./components/overview.component";
+import {CouponComponent} from "./components/coupon.component";
+import {ImpressumComponent} from "./components/impressum.component";
+import {DatenschutzComponent} from "./components/datenschutz.component";
+import {ProductDetailsComponent} from "./components/product-details.component";
+import {LandingPageComponent} from "./components/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview/:categorie/:subcategorie', component: OverviewComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'overview/:brand', component: OverviewComponent },
+  { path: 'overview/:product', component: OverviewComponent },
+  { path: 'coupons', component: CouponComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'datenschutz', component: DatenschutzComponent },
+  { path: 'details/:id', component: ProductDetailsComponent },
+  { path: 'home', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './components/overview.component';
+import { CouponComponent } from './components/coupon.component';
+import { ImpressumComponent } from './components/impressum.component';
+import { DatenschutzComponent } from './components/datenschutz.component';
+import { ProductDetailsComponent } from './components/product-details.component';
+import { LandingPageComponent } from './components/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    CouponComponent,
+    ImpressumComponent,
+    DatenschutzComponent,
+    ProductDetailsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
